279d18e9c3ec1d2ac64222bb90140f7f6ee7636f
Uroš Polanc <up4473@gmail.com>
2015-10-17 19:03:45 +0000

    Commit #001

---
+++ script/matrix.js
@@ -0,0 +1,27 @@
+function Matrix4f () {
+    
+}
+
+Matrix4f.prototype.negate = function (matrix) {
+    
+};
+
+Matrix4f.prototype.add = function (matrix_1, matrix_2) {
+    
+};
+
+Matrix4f.prototype.transpose = function (matrix) {
+    
+};
+
+Matrix4f.prototype.multiply_scalar = function (matrix, value) {
+    
+};
+
+Matrix4f.prototype.multiply_matrix = function (matrix_1, matrix_2) {
+    
+};
+
+Matrix4f.prototype.display = function (matrix) {
+    
+};
---
+++ script/transformation.js
@@ -0,0 +1,27 @@
+function Transformatoion () {
+    
+}
+
+Transformatoion.prototype.translate = function () {
+    
+};
+
+Transformatoion.prototype.scale = function () {
+    
+};
+
+Transformatoion.prototype.rotate_x = function () {
+    
+};
+
+Transformatoion.prototype.rotate_y = function () {
+    
+};
+
+Transformatoion.prototype.rotate_z = function () {
+    
+};
+
+Transformatoion.prototype.transform = function () {
+    
+};
